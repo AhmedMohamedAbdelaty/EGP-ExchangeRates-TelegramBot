f0960ef3dbcf55a835b536d1c9cff679b36a55df
Ahmed Mohamed <am0103738@gmail.com>
2024-02-08 15:09:42 +0200

    Update classpath in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Compile the Java files
-javac -cp .:lib/telegrambots-6.9.7.0.jar:lib/jsoup-1.14.2.jar src/main/java/*.java
+javac -cp ".:lib/*" src/main/java/*.java
 # Run the main class
-java -cp .:lib/telegrambots-6.9.7.0.jar:lib/jsoup-1.14.2.jar:src/main/java Main
+java -cp ".:lib/*" Main
